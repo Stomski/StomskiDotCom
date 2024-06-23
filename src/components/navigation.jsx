478c45e665f9ff98759ebf1c718683d291186ced
Robert Stomski <stomski.robert@gmail.com>
2024-06-22 22:34:48 -0600

    build ran, testing pulldown close on selection

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 
 export const Navigation = (props) => {
+  const handleNavItemClick = () => {
+    const navBarCollapse = document.getElementById(
+      "bs-example-navbar-collapse-1"
+    );
+    if (navBarCollapse.classList.contains("in")) {
+      // For Bootstrap 3
+      navBarCollapse.classList.remove("in");
+      navBarCollapse.style.height = "0px";
+    }
+  };
+
   return (
     <nav id="menu" className="navbar navbar-default navbar-fixed-top">
       <div className="container">
@@ -18,7 +29,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            Bobby Stomski <span class="nowrap">WEB DEVELOPMENT</span>
+            Bobby Stomski <span className="nowrap">WEB DEVELOPMENT</span>
           </a>{" "}
         </div>
 
@@ -28,28 +39,47 @@ export const Navigation = (props) => {
         >
           <ul className="nav navbar-nav navbar-right">
             <li>
-              <a href="#about" className="page-scroll">
+              <a
+                href="#about"
+                className="page-scroll"
+                onClick={handleNavItemClick}
+              >
                 About
               </a>
             </li>
             <li>
-              <a href="#services" className="page-scroll">
+              <a
+                href="#services"
+                className="page-scroll"
+                onClick={handleNavItemClick}
+              >
                 Tech stack
               </a>
             </li>
             <li>
-              <a href="#portfolio" className="page-scroll">
+              <a
+                href="#portfolio"
+                className="page-scroll"
+                onClick={handleNavItemClick}
+              >
                 Project Gallery
               </a>
             </li>
             <li>
-              <a href="#testimonials" className="page-scroll">
+              <a
+                href="#testimonials"
+                className="page-scroll"
+                onClick={handleNavItemClick}
+              >
                 Testimonials
               </a>
             </li>
-
             <li>
-              <a href="#contact" className="page-scroll">
+              <a
+                href="#contact"
+                className="page-scroll"
+                onClick={handleNavItemClick}
+              >
                 Contact
               </a>
             </li>
